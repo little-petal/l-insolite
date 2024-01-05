ddf36a24eb7967df95a92de35eafe997cc23dcba
little-petal <julie.falque@protonmail.com>
2024-01-05 22:39:46 +0100

    change prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,13 @@
 import { WriteItem } from '@/types/WriteItem';
 import { Item, PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-declare global {
-    var prisma: PrismaClient;
-}
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global!.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export async function searchAllItems() {
     const items = await prisma.item.findMany({
