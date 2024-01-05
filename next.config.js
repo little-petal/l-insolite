4f4abad657a656600bac4738619956f58fda4c48
little-petal <julie.falque@protonmail.com>
2024-01-05 20:16:55 +0100

    idem

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const nextConfig = {
               headers: [
                   { key: "Access-Control-Allow-Credentials", value: "true" },
                   { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                  { key: "Access-Control", value: "*" },
                   { key: "Access-Control-Allow-Methods", value: "GET" },
                   { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
               ]
